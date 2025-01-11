8908a30b20e2c3884259a730dd3b1a93a63df001
JefferNg <jefferng11@gmail.com>
2025-01-10 16:10:52 -0800

    changed layout and added text

--- Lab1GradRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ Lab1GradRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -16,6 +26,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
 
@@ -37,20 +48,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.grad_cap),
-        contentDescription = stringResource(R.string.graduation_cap),
-        alpha = 0.7f
-    )
-    Text(
-        text = stringResource(R.string.graduation_announcement),
-        fontSize = 50.sp,
+    val padding = 50.dp
+    Column(
         modifier = modifier,
-        lineHeight = 50.sp,
-        fontWeight = FontWeight.Bold,
-        color = Color.Red,
-        textAlign = TextAlign.Center
-    )
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.size(padding))
+        Text(
+            text = stringResource(R.string.graduation_announcement),
+            fontSize = 50.sp,
+            modifier = modifier,
+            lineHeight = 50.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            textAlign = TextAlign.Center
+        )
+        Spacer(Modifier.size(padding))
+        Image(
+            painter = painterResource(R.drawable.grad_cap),
+            contentDescription = stringResource(R.string.graduation_cap),
+            alpha = 0.7f
+        )
+        Spacer(Modifier.size(padding))
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Icon(Icons.Filled.Info, contentDescription = "Alert Symbol")
+            Text(
+                text = "May 14 - Ganus Hall - 2:00 pm"
+            )
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
