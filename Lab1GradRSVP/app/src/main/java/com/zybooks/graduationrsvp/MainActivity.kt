3845788791f433b4560dad9d4de0e54f83dca641
JefferNg <jefferng11@gmail.com>
2025-01-15 14:29:51 -0800

    implemented switch button

--- Lab1GradRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ Lab1GradRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
@@ -16,8 +17,13 @@ import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -79,9 +85,34 @@ fun GradScreen(modifier: Modifier = Modifier) {
                 text = "May 14 - Ganus Hall - 2:00 pm"
             )
         }
+        Spacer(Modifier.height(padding))
+        Rsvp(modifier)
 
     }
 
+}
+
+@Preview
+@Composable
+fun Rsvp(modifier: Modifier = Modifier) {
+    var isChecked by remember { mutableStateOf(true) }
+    val message = if (isChecked) "I'll be there!" else "Sorry I can't be there."
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Switch(
+            checked = isChecked,
+            onCheckedChange = {
+                isChecked = it
+            }
+        )
+        Text(
+            text = message,
+            fontSize = 30.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
